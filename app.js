ec4c3a008b3548fed84adc0b9c570e63a932526c
Dhaval Chauhan <dhavalchauhan171994@gmail.com>
2017-01-22 22:58:08 +0530

    cron

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
-//var express = require('express')
 const publicIp = require('public-ip');
 const exec = require('child_process').exec;
+var CronJob = require('cron').CronJob;
 
 var ipv6s = [
 	"list",
 	"biglist"
 ];
 
+//Randomize every second
 
-publicIp.v6().then(ip => {
-    exec('ip -6 addr del ' + ip + '/64 dev eth0', (error, stdout, stderr) => {
-		});
-		var count = Math.floor(Math.random() * ipv6s.length) + 1;
-		if(count == ipv6s.length) {
-			count = 0;
-		}
-		else {
-			count++;
-		}
-		exec('ip -6 addr add ' + ipv6s[count] + '/64 dev eth0', (error, stdout, stderr) => {
-		});
-});
-
-publicIp.v6().then(ip => {
-    console.log("New IP " + ip);
-});
+new CronJob('* * * * * *', function() {
+	
+	publicIp.v6().then(ip => {
+	    exec('ip -6 addr del ' + ip + '/64 dev eth0', (error, stdout, stderr) => {
+			});
+			var count = Math.floor(Math.random() * ipv6s.length) + 1;
+			if(count == ipv6s.length) {
+				count = 0;
+			}
+			else {
+				count++;
+			}
+			exec('ip -6 addr add ' + ipv6s[count] + '/64 dev eth0', (error, stdout, stderr) => {
+			});
+	});
+	
+	publicIp.v6().then(ip => {
+	    console.log("New IP " + ip);
+	});
+	
+}, null, true, 'America/Los_Angeles');
