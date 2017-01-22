e57ff260d4523d1fd2ed8ee5ff22a11aaaed9af6
Dhaval Chauhan <dhavalchauhan171994@gmail.com>
2017-01-22 23:05:31 +0530

    cron

--- app.js
+++ app.js
@@ -3,30 +3,23 @@ const exec = require('child_process').exec;
 var CronJob = require('cron').CronJob;
 
 var ipv6s = [
-	"list",
-	"biglist"
+    "list",
+    "biglist"
 ];
 
 //Randomize every second
 
 new CronJob('* * * * * *', function() {
-	
-	publicIp.v6().then(ip => {
-	    exec('ip -6 addr del ' + ip + '/64 dev eth0', (error, stdout, stderr) => {
-			});
-			var count = Math.floor(Math.random() * ipv6s.length) + 1;
-			if(count == ipv6s.length) {
-				count = 0;
-			}
-			else {
-				count++;
-			}
-			exec('ip -6 addr add ' + ipv6s[count] + '/64 dev eth0', (error, stdout, stderr) => {
-			});
-	});
-	
-	publicIp.v6().then(ip => {
-	    console.log("New IP " + ip);
-	});
-	
+
+    publicIp.v6().then(ip => {
+        exec('ip -6 addr del ' + ip + '/64 dev eth0', (error, stdout, stderr) => {});
+        count = Math.floor(Math.random() * ipv6s.length) + 1;
+
+        exec('ip -6 addr add ' + ipv6s[count] + '/64 dev eth0', (error, stdout, stderr) => {});
+    });
+
+    publicIp.v6().then(ip => {
+        console.log("New IP " + ip);
+    });
+
 }, null, true, 'America/Los_Angeles');
